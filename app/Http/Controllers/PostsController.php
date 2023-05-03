2effda3e027abcfc31c73aada16a045c79f63f03
Wevioz <wevioz@mail.ru>
2023-05-03 14:55:28 +0300

    Done the styling of the posts main page

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -8,13 +8,14 @@ class PostsController extends Controller
 {
     function showMain() {
         $title = "Finsweet";
+        $posts = [1, 2, 3, 4, 5, 6];
     
-        return view("posts.list", compact("title"));
+        return view("posts.list", compact("title", "posts"));
     }
 
     function showInfo() {
         $title = "Post name";
-    
+
         return view("posts.info", compact("title"));
     }
 }
---
+++ resources/views/posts/post.blade.php
@@ -0,0 +1,11 @@
+<a href="{{route("info-page", ["id" => 1])}}" class="post">
+    <img src="img/mock-post.jpg" alt="Post Picture" width="490" height="318" class="post__picture">
+    <div class="post__right-side">
+        <p class="post__category">Startup</p>
+        <h3 class="post__title">Design tips for designers that cover everything you need</h3>
+        <p class="post__description">
+            Duis aute irure dolor in reprehenderit in voluptate velit
+             esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident.
+        </p>
+    </div>
+</a>
